d841a41a839870b7fade01e4786e56ce198b7464
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2020-09-04 13:14:29 -0400

    commit with completed HTML

---
+++ Assets/JS/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    // var scheduleHour = $(".hour").attr("id");
+    // var scheduleTime = scheduleHour.slice(-2);
+    // var description = $(".description").val;
+    // var currentHour = moment().hour();
+
+    console.log($(".hour"))
+
+//     // display current day, date, and time
+//     $("#currentDay").text(moment().format("dddd, MMMM do YYYY, hh:mm A"))
+
+//     // function to class description blocks
+//     $(".hour")
+
+//     // check current time of day in hours to class description block
+        
+//     console.log(currentHour)
+//     console.log(typeof(currentHour))
+
+    
+
+//     // function to read what's stored in local storage and display it
+
+
+//     $(".saveBtn").on("click", function() {
+//         localStorage.setItem(scheduleHour, description)
+
+//     })
+
+//     console.log(scheduleHour)
+//     console.log(description)
+
+//     $(".description").localStorage.getItem(scheduleHour)
+})
